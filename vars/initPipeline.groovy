c0db73c1f20c30051c3e848598a22e36bfdd842e
BasmaBoulekhras <basmaboulekhras@gmail.com>
2018-11-07 13:20:03 -0500

    Update and rename hello_world.groovy to initPipeline.groovy

---
+++ vars/initPipeline.groovy
@@ -0,0 +1,11 @@
+#!/usr/bin/groovy
+
+def call() {
+	def payloadString = "$payload"
+	parseWebhookPayload payload: payloadString
+	
+	currentBuild.displayName = repoName + ":" + branch + " #" + BUILD_NUMBER
+	currentBuild.description = "Building repository: " + repoName + ", branch: " + branch
+	
+	slack.info message: 'STARTED', appendBuildInfo: true
+}
