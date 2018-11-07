6c57893662130222b389d4e02142cb6af43ed326
BasmaBoulekhras <basmaboulekhras@gmail.com>
2018-11-07 16:13:59 -0500

    Update deploymentLogic.groovy

--- vars/deploymentLogic.groovy
+++ vars/deploymentLogic.groovy
@@ -1,32 +1,36 @@
 #!/usr/bin/groovy
 
 def call(namespace) {
+ 
+ def project = 'green-dispatch-219519'
+ def  appName = 'sample-app'
+ def  imageTag = "gcr.io/${project}/${appName}:${env.BRANCH_NAME}.${env.BUILD_NUMBER}"
 
-switch (namespace){
- case "canary":
+ switch (namespace){
+  case "canary":
 
-   sh("sed -i.bak 's#gcr.io/green-dispatch-219519/sample-app:v1#${imageTag}#' ./k8s/canary/*.yaml")
+    sh("sed -i.bak 's#gcr.io/green-dispatch-219519/sample-app:v1#${imageTag}#' ./k8s/canary/*.yaml")
          
-   sh("kubectl --namespace=canary apply -f k8s/canary/")
+    sh("kubectl --namespace=canary apply -f k8s/canary/")
    
-   break
+    break
           
- case "production":
+  case "production":
 
-   sh("sed -i.bak 's#gcr.io/green-dispatch-219519/sample-app:v1#${imageTag}#' ./k8s/production/*.yaml")
+    sh("sed -i.bak 's#gcr.io/green-dispatch-219519/sample-app:v1#${imageTag}#' ./k8s/production/*.yaml")
          
-   sh("kubectl --namespace=production apply -f k8s/production/")
+    sh("kubectl --namespace=production apply -f k8s/production/")
 
-   break
+    break
    
- default:  
+  default:  
    // Create namespace if it doesn't exist
-   sh("kubectl get ns ${env.BRANCH_NAME} || kubectl create ns ${env.BRANCH_NAME}")
+    sh("kubectl get ns ${env.BRANCH_NAME} || kubectl create ns ${env.BRANCH_NAME}")
    //Don't use public load balancing for development branches
-   sh("sed -i.bak 's#gcr.io/green-dispatch-219519/sample-app:v1#${imageTag}#' ./k8s/dev/*.yaml")  
-   sh("kubectl --namespace=${env.BRANCH_NAME} apply -f k8s/dev/")
+    sh("sed -i.bak 's#gcr.io/green-dispatch-219519/sample-app:v1#${imageTag}#' ./k8s/dev/*.yaml")  
+    sh("kubectl --namespace=${env.BRANCH_NAME} apply -f k8s/dev/")
           
-   break       
+    break       
 }    
 
 }
