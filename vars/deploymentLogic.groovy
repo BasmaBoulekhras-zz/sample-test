1c62dff917e74308daf8cc1513bb9e9b4b4459da
BasmaBoulekhras <basmaboulekhras@gmail.com>
2018-11-07 15:30:21 -0500

    Update deploymentLogic.groovy

--- vars/deploymentLogic.groovy
+++ vars/deploymentLogic.groovy
@@ -19,7 +19,7 @@ switch (namespace){
 
    break
    
- default
+ default:  
    // Create namespace if it doesn't exist
           //sh("kubectl get ns ${env.BRANCH_NAME} || kubectl create ns ${env.BRANCH_NAME}")
           // Don't use public load balancing for development branches
