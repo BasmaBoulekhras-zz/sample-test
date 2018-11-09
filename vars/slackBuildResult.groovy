2657732307dd6f1c7372779635e93c73f5c156b4
BasmaBoulekhras <basmaboulekhras@gmail.com>
2018-11-09 16:52:36 -0500

    Create slackBuildResult.groovy

---
+++ vars/slackBuildResult.groovy
@@ -0,0 +1,17 @@
+#!/usr/bin/groovy
+
+def call() {
+	
+	if ( currentBuild.currentResult == "SUCCESS") {
+		slack.success message: currentBuild.currentResult, appendBuildInfo: true
+	}
+	else if( currentBuild.currentResult == "UNSTABLE" || currentBuild.currentResult == "ABORTED" ) { 
+		slack.warning message: currentBuild.currentResult, appendBuildInfo: true
+	}
+	else if( currentBuild.currentResult == "FAILURE" || currentBuild.currentResult == "NOT_BUILT" ) { 
+		slack.failure message: currentBuild.currentResult, appendBuildInfo: true
+	}
+	else {
+		slack.failure message: currentBuild.currentResult, appendBuildInfo: true
+	}
+}
